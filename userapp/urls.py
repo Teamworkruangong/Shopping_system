2b338474c765e4b188cca1f47066dabc3ae9887c
qilong li <97083300+L7L9@users.noreply.github.com>
2023-11-26 20:06:16 +0800

    feat(userapp):day3,添加登录页面和view层代码

--- userapp/urls.py
+++ userapp/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns=[
     url(r'^register/$',views.RegisterView.as_view()),
     url(r'^center/$',views.centerView),
+    url(r'^login/$',views.LoginView.as_view()),
+    url(r'^loadCode/$',views.LoadCodeView.as_view()),
+    url(r'^checkCode/$',views.CheckCodeView.as_view()),
+    url(r'^logout/$',views.LogOutView.as_view()),
+    url(r'^address/$',views.AddressView.as_view()),
+    url(r'^loadArea/$',views.loadAreaView),
+
 
 ]
