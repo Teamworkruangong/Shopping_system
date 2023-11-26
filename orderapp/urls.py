1b24906e1bee760ff5285c415ac634e37f5291b8
h126825147 <109055961+h126825147@users.noreply.github.com>
2023-11-26 20:19:31 +0800

    配送方式选择功能的实现

--- orderapp/urls.py
+++ orderapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.toOrderView)
+    url(r'^$',views.toOrderView),
+    url(r'^toPay/$',views.toPayView),
+    url(r'^checkPay/$',views.checkPayView),
 
 ]
--- orderapp/views.py
+++ orderapp/views.py
@@ -1,9 +1,15 @@
+import uuid
+
+import datetime
 import jsonpickle
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from cartapp.cartmanager import DBCartManger
+from orderapp.models import Order, OrderItem
+from userapp.models import Address
+from utils.alipay_p3 import AliPay
 
 
 def toOrderView(request):
@@ -39,3 +45,68 @@ def toOrderView(request):
 
 
     return render(request,'order.html',{'addrObj':addrObj,'cartItemObjList':cartItemObjList,'totalPrice':totalPrice})
+
+
+alipayObj = AliPay(appid='2016091100486702', app_notify_url='http://127.0.0.1:8000/order/checkPay/', app_private_key_path='orderapp/keys/my_private_key.txt',
+                 alipay_public_key_path='orderapp/keys/alipay_public_key.txt', return_url='http://127.0.0.1:8000/order/checkPay/', debug=True)
+
+
+def toPayView(request):
+
+    #获取请求参数
+    addrid = request.GET.get('address',-1)
+    payway = request.GET.get('payway','alipay')
+    cartitems = request.GET.get('cartitems','')
+
+
+    params = {
+        'out_trade_num':uuid.uuid4().hex,
+        'order_num':datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
+        'address':Address.objects.get(id=addrid),
+        'user':jsonpickle.loads(request.session.get('user', '')),
+        'payway':payway
+    }
+
+
+
+    orderObj = Order.objects.create(**params)
+
+    # '['{'goodsid:1','sizeid:'2',...'}']'
+    if cartitems:
+        #[{dict1},{dict2}]
+        cartitems = jsonpickle.loads(cartitems)
+
+        orderItemList = [OrderItem.objects.create(order=orderObj,**ci) for ci in cartitems if ci]
+
+
+    urlparam = alipayObj.direct_pay(subject='京东商城', out_trade_no=orderObj.out_trade_num, total_amount=request.GET.get('totalPrice',0))
+
+
+    url = alipayObj.gateway+'?'+urlparam
+
+    return HttpResponseRedirect(url)
+
+
+def checkPayView(request):
+    #获取所有的请求参数
+    params = request.GET.dict()
+
+    #获取签名
+    sign = params.pop('sign')
+
+    #进行校验
+    if alipayObj.verify(params,sign):
+        # 修改订单状态
+
+
+        #修改库存
+
+
+
+        #更新购物车表中数据
+
+
+
+        return HttpResponse('支付成功！')
+
+    return HttpResponse('支付失败！')
